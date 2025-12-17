1c82762f194e887ad807d0309c19f5b895b01976
DimasPra27 <dimasprasetyo2708@gmail.com>
2025-12-18 01:20:19 +0700

    Fix position card form

--- app/form/page.tsx
+++ app/form/page.tsx
@@ -64,86 +64,90 @@ export default function Form() {
   return (
     <>
       {/* Desktop Layout */}
-      <Card className="hidden lg:block w-full max-w-screen-xl lg:mx-230 lg:w-[700px] p-4 rounded-3xl mt-15 lg:mt-30">
-        <div className="flex flex-col items-center justify-center lg:justify-between sm:gap-5 gap-3 sm:py-10 lg:py-0">
-          <div className="flex flex-col gap-3 items-center text-center flex-1 justify-center lg:mt-0 min-h-[auto]">
-            <h1
-              className="text-3xl lg:text-5xl text-white"
-              style={{ fontFamily: "'pulang', sans-serif" }}
-            >
-              Selamat Datang!
-            </h1>
-            <p className="text-base lg:text-xl font-normal text-white">
-              Temukan kenagan manis bersama kami! Isi formulir singkat ini dan
-              bagikan pengalaman Anda menikmati kue soes kami
-            </p>
-          </div>
+      <div className="hidden lg:flex justify-end items-start pt-8 pr-8">
+        <Card className="w-full max-w-md lg:px-12 lg:py-8 p-4 rounded-3xl">
+          <div className="flex flex-col items-center justify-center gap-5">
+            <div className="flex flex-col gap-3 items-center text-center">
+              <h1
+                className="text-3xl lg:text-5xl text-white"
+                style={{ fontFamily: "'pulang', sans-serif" }}
+              >
+                Selamat Datang!
+              </h1>
+              <p className="text-base lg:text-xl font-normal text-white">
+                Temukan kenagan manis bersama kami! Isi formulir singkat ini dan
+                bagikan pengalaman Anda menikmati kue soes kami
+              </p>
+            </div>
 
-          <div className="w-full shrink-0">
-            <div className="space-y-6">
-              <div>
-                <Input
-                  type="text"
-                  className="bg-[#FFB5DA] placeholder-[#096831] text-sm border-1 border-[#FFB5DA] h-10 focus:ring-2 focus:ring-[#FFB5DA] focus:border-[#FFB5DA] text-[#096831] rounded-3xl"
-                  placeholder="Nama"
-                  id="name"
-                  value={form.name}
-                  onChange={handleChange}
-                />
-                {errors.name && (
-                  <p className="text-green-500 text-sm">{errors.name}</p>
-                )}
-              </div>
+            <div className="w-full">
+              <div className="space-y-6">
+                <div>
+                  <Input
+                    type="text"
+                    className="bg-[#FFB5DA] placeholder-[#096831] text-sm border-1 border-[#FFB5DA] h-10 focus:ring-2 focus:ring-[#FFB5DA] focus:border-[#FFB5DA] text-[#096831] rounded-3xl"
+                    placeholder="Nama"
+                    id="name"
+                    value={form.name}
+                    onChange={handleChange}
+                  />
+                  {errors.name && (
+                    <p className="text-green-500 text-sm mt-1">{errors.name}</p>
+                  )}
+                </div>
 
-              <div>
-                <Input
-                  type="text"
-                  className="bg-[#FFB5DA] placeholder-[#096831] text-sm border-2 border-[#FFB5DA] h-10 focus:ring-2 focus:ring-[#FFB5DA] focus:border-[#FFB5DA] text-[#096831] rounded-3xl"
-                  placeholder="+081xxxxxxxx"
-                  id="phoneNumber"
-                  value={form.phoneNumber}
-                  onChange={handleChange}
-                />
-                {errors.phoneNumber && (
-                  <p className="text-green-500 text-sm">{errors.phoneNumber}</p>
-                )}
-              </div>
+                <div>
+                  <Input
+                    type="text"
+                    className="bg-[#FFB5DA] placeholder-[#096831] text-sm border-2 border-[#FFB5DA] h-10 focus:ring-2 focus:ring-[#FFB5DA] focus:border-[#FFB5DA] text-[#096831] rounded-3xl"
+                    placeholder="+081xxxxxxxx"
+                    id="phoneNumber"
+                    value={form.phoneNumber}
+                    onChange={handleChange}
+                  />
+                  {errors.phoneNumber && (
+                    <p className="text-green-500 text-sm mt-1">
+                      {errors.phoneNumber}
+                    </p>
+                  )}
+                </div>
 
-              <div>
-                <textarea
-                  name="message"
-                  id="comment"
-                  placeholder="Enak banget kue soes nya..."
-                  className="h-20 bg-[#FFB5DA] placeholder-[#096831] border-2 border-[#FFB5DA] text-[#096831] text-sm focus:ring-2 focus:ring-[#FFB5DA] focus:border-[#FFB5DA] block w-full p-2.5 rounded-3xl"
-                  value={form.comment}
-                  onChange={handleChange}
-                ></textarea>
-              </div>
+                <div>
+                  <textarea
+                    name="message"
+                    id="comment"
+                    placeholder="Enak banget kue soes nya..."
+                    className="h-20 bg-[#FFB5DA] placeholder-[#096831] border-2 border-[#FFB5DA] text-[#096831] text-sm focus:ring-2 focus:ring-[#FFB5DA] focus:border-[#FFB5DA] block w-full p-2.5 rounded-3xl"
+                    value={form.comment}
+                    onChange={handleChange}
+                  ></textarea>
+                </div>
 
-              <div className="flex justify-center">
-                <StarRating
-                  value={form.rate}
-                  onChange={(value) =>
-                    setForm({
-                      ...form,
-                      rate: value,
-                    })
-                  }
-                />
-              </div>
+                <div className="flex justify-center">
+                  <StarRating
+                    value={form.rate}
+                    onChange={(value) =>
+                      setForm({
+                        ...form,
+                        rate: value,
+                      })
+                    }
+                  />
+                </div>
 
-              <div className="flex justify-center">
-                <button
-                  onClick={handleSubmit}
-                  className="px-30 py-2 text-base font-bold text-[#0C7A3F] rounded-full bg-[#FFFFFF] hover:bg-gray-100 focus:ring-4 focus:ring-emerald-300 transition-all shadow-md"
-                >
-                  Masuk
-                </button>
+                <div className="flex justify-center">
+                  <button
+                    onClick={handleSubmit}
+                    className="px-20 py-2 text-base font-bold text-[#0C7A3F] rounded-full bg-[#FFFFFF] hover:bg-gray-100 focus:ring-4 focus:ring-emerald-300 transition-all shadow-md"
+                  >
+                    Masuk
+                  </button>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </Card>
+        </Card>
+      </div>
 
       {/* Mobile Layout */}
       <div className="lg:hidden fixed bottom-0 left-0 right-0 bg-[#0C7A3F] rounded-t-[6.5rem] px-6 pt-8 pb-6 shadow-2xl z-50">
